81a62db861f2be9aa48914d2dd1ac3a243fbcdfd
swathi-mindfire <swathi@mindfiresolutions.com>
2021-03-13 01:42:08 +0530

    changed to full responsive

---
+++ js/form-validation.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    
+   
+
+   /* var countryList = (function(){
+        var countries = ["Australia","America","Canada","India","Romainia","Russia","Switzerland",]
+        function generateCountries(){
+            var country = $("#country1");
+            alert(country.val())
+           // country.length=0;
+            //country.options[0] = new Option('select Country', '0');
+            country.selectedIndex = 0;
+            for(var i=0; i< countries.length;i++){
+            country.options[i+1] = new Option(countries[i],countries[i]);
+            }
+        }
+        return {generateCountries:generateCountries};
+    })();
+
+
+    alert(countryList)
+    var countryl = countryList();
+    countryl.generateCountries(); */
+
+   
+
+  
+
+
+
+    /*var validate = function(){
+
+       
+
+        function formfields(){
+           alert("hi")
+            
+        }
+        return {formfields:formfields}
+    }
+    var v1 = validate();
+    v1.formfields();*/
+
+   
+
+   
+    
+
+})
+
+/*grp1 = function(){
+
+    let 
+
+
+}*/
