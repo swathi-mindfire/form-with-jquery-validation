3c959b7e25d9f6fecd199a33d87d4a93e5697657
swathi-mindfire <swathi@mindfiresolutions.com>
2021-03-14 15:14:30 +0530

    revealing module pattern used for validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         var grp2 =  $("#grp2");
         var grp1 =  $("#grp1");
         var prev  = $("#prev");
+        var addressflag,countryflag,stateflag,cityflag,resflag;
+        addressflag = countryflag = stateflag = cityflag = resflag =true;
         //checking empty input fields 
         function noValueError(pos){
             var pos1 = "#" + pos ;
@@ -19,8 +21,14 @@ $(document).ready(function(){
                 }
             }
         }
-        //First group  validation,if it success then only  next page opens
+        //Go to previous section
 
+        function showGrp1(){
+            grp2.fadeOut(3000,function(){
+                grp1.fadeIn()
+            })
+        }
+        //First group  validation,if it success then only  next page opens
         function checkGrp1(){
             var fname = $("#firstname");            
             var lname = $("#lastname");               
@@ -40,12 +48,10 @@ $(document).ready(function(){
             var pwdreg = /^([!,.+-{}()[]~`%#:;]+)$/;
             var fnflag,lnflag,mlflag,m1flag,p1flag,p2flag,genflag;
             fnflag = lnflag = mlflag = m1flag = p1flag = p2flag = genflag= true;
-
              var namecheck = (function(){
                  //First name validation
                  function fnamechk(){
-                    if((fname.val()).trim() == ""){
-                        
+                    if((fname.val()).trim() == ""){                        
                           fnerror.text("please enter first name");
                           fnflag = false;
                           fname.focus();
@@ -55,11 +61,8 @@ $(document).ready(function(){
                     }
                     else{
                         if ((fname.val()).length <3 ){
-                            fnflag = false;
-                           
-            
-                            fnerror.text("Name is minimun 3 characters");
-                            
+                            fnflag = false;           
+                            fnerror.text("Name is minimun 3 characters");                            
                             fname.focus();
                             fname.change(function() {
                                 fnerror.text(" ");
@@ -111,8 +114,6 @@ $(document).ready(function(){
 
                 }
             })();
-
-
             mailchk = (function(){
                 //Email validation
                 function checkemail(){
@@ -169,7 +170,6 @@ $(document).ready(function(){
                             }
 
                         }*/  
-
                     }
                 }
                 //passwords  match checking
@@ -263,8 +263,6 @@ $(document).ready(function(){
                     grp1FieldsCompletenessCheck : grp1FieldsCompletenessCheck
                 };
             })();
-
-
             var validatename = namecheck;
             validatename.fnamechk();
             validatename.lnamechk();
@@ -281,7 +279,6 @@ $(document).ready(function(){
             showGrp2. grp1FieldsCompletenessCheck();
         }
         //Group 2 validation
-
         function generateCountries(){
             var countries = ["Australia","America","Canada","India","Romainia","Russia","Switzerland",]
             var country1 = document.getElementById("country1");
@@ -297,84 +294,232 @@ $(document).ready(function(){
         }
         function generateCaptcha(){
             var expr= $('#exprsn');
-
             var num1= Math.round((100-10)*Math.random() +10 );
-
             var num2= Math.round((100-10)*Math.random() + 10);
-
             var operations = ['*','/','+','-']
-
             var x= (Math.round(10*Math.random()))%4;
-
             var op =operations[x];
-
             var compute = num1 + op  + num2
-
             expr.val(compute)
         }
-
         function getStates(country,state){
             var states = new Array();
             states[0] = "slelct state";
-            states[1] = "New South Wales,Queensland,South Australia,Tasmania,Victoria,Western Australia"
-            states[2] = "Alabama,Alaska,California,Colorado,Florida,Georgia, Guam, Hawaii,New Jersey,South Carolina, South Dakota, Tennessee, Texas, U.S. Virgin Islands, Utah, Vermont, Virginia, Washington";
-            states[3] =  "Alberta, British Columbia, Manitoba, New Brunswick,Quebec, Saskatchewan, Yukon";
+            states[1] = "Australian Capital Territory,New South Wales,Northern Territory,Queensland,South Australia,Tasmania,Victoria,Western Australia";
+            states[2] =  "Alabama.Alaska,Arizona,Arkansas,California,Colorado,Connecticut,Delaware,District of Columbia,Florida,Georgia,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,Louisiana,Maine,Mississippi,Missouri,Montana,Nebraska,Nevada,New Hampshire,New Jersey,New Mexico,New York,North Carolina,North Dakota,Ohio,Oklahoma,tah,Vermont,Virginia,Washington,WestVirginia,Wisconsin,Wyoming";
+            states[3] =  "Alberta,British Columbia,Manitoba,New Brunswick,Newfoundland,Northwest Territories,Nova Scotia,Nunavut,Ontario,Prince Edward Island,Quebec,Saskatchewan,Yukon Territory";
             states[4] = "Andhra Pradesh,Arunachal Pradesh,Assam,Bihar,Chhattisgarh,Goa,Gujarat,Haryana,Himachal Pradesh,Jammu and Kashmir,Jharkhand,Karnataka,Kerala,Madhya Pradesh,Maharashtra,Manipur,Meghalaya,Mizoram,Nagaland,Odisha,Punjab,Rajasthan,Sikkim,Tamil Nadu,Telangana,Tripura,Uttar Pradesh,Uttarakhand,West Bengal,Andaman and Nicobar,Chandigarh,Dadra and Nagar Haveli,Daman and Diu,Lakshadweep,Delhi,Puducherry"
-            states[5]  = "Arges,Bacau,Bihor,Bistrita-Nasaud,Botosani,Braila,Brasov,Bucuresti,Buzau,Calarasi";
-            states[6] = "Adygeya,Aginskiy,Buryatskiy,Altay (Gorno-Altaysk),Altayskiy";
-            states[7] = "Aargau,Ausser-Rhoden,Basel-Landschaft,Basel-Stadt,Bern,Fribourg,Geneve,Glarus,Graubunden"
+            states[5]  = "Alba,Arad,Arges,Bacau,Bihor,Bistrita-Nasaud,Botosani,Braila,Brasov,Bucuresti,Buzau,Calarasi,Caras-Severin,Cluj,Constanta,Covasna,Dimbovita,Dolj,Galati,Giurgiu,Gorj,Harghita,Hunedoara,Ialomita";
+            states[6] = "Adygeya,Aginskiy Buryatskiy,Altay,Altayskiy,Amurskaya,Arkhangel'skaya,Astrakhanskaya,Bashkortostan,Belgorodskaya,Bryanskaya,Buryatiya,Chechnya,Chelyabinskaya,Chitinskaya,Chukotskiy";
+            states[7] = "Aargau,Ausser-Rhoden,Basel-Landschaft,Basel-Stadt,Bern,Fribourg,Geneve,Glarus,Graubunden,Inner-Rhoden,Jura,Luzern,Neuchatel"
             var indx = document.getElementById(country).selectedIndex;
             var st = document.getElementById(state);
             var s_arr = states[indx].split(",");
-            st.options[0] = new Option("Select State", "0");
+            st.options[0] = new Option('Select State', '0');
             var s_arr = states[indx].split(",");
 
             for(var i=0;i<s_arr.length;i++){
                 st.options[i+1] = new Option(s_arr[i],s_arr[i]);
             }
-
         }
-
         function grp2Validate(){
             cityreg = /^[a-zA-Z]+$/;    
             addrreg = /^[a-zA-Z0-9,-]+$/;  
-            var caddress = $("#address");
+            var addr1 = $("#address");
             var ccountry = $("#country1");
             var cstate = $("#state1");
             var city1 = $("#cityname");
-            var pcountry = $("#country2");
-            var cstate = $("#state2");
+            var a1error = $("#addresserror");
+            var c1error = $("#country1error");
+            var s1error = $("#state1error");
+            var cterror  = $("#citynameerror")              
+            addressValidate = (function(){
+                 //Address validation
+                 function checkaddress(){
+                    if((addr1.val()).trim() == ""){
+                        addressflag = false;
+                        
+                        a1error.text("please enter Your Address");
+                        addr1.focus();
+                        addr1.change(function() {
+                            a1error.text(" ");
+                        });           
+                    }
+                    else if (!(addr1.val()).match(cityreg)){ 
+                        addressflag = false;            
+                        a1error.text("special characters not allowed")
+                        a1error.focus();
+                        addr1.change(function() {
+                            a1error.text(" ");
+                        });                                
+                    }
+                    else{
+                        addressflag = true;
 
+                    }
+
+                }return {
+                    checkaddress : checkaddress
+
+                };
+                
+            })();
+
+            countryStateValidate = (function(){
+                //Address validation
+                function checkCountry(){
+                    //country selection checking
+                    if(ccountry.val()== 0){
+                        countryflag = false;
+                        c1error.text("please selecty your country");
+                        ccountry.focus();
+                        ccountry.change(function() {
+                            c1error.text(" ");
+                        });           
+                    }
+                    else{
+                        countryflag = true;
+
+                    }
+                }
+                function checkState(){
+                     //state validation
+                     if(cstate.val()== 0){
+                        stateflag = false;
+                        s1error.text("please select your State");
+                        cstate.focus();
+                        cstate.change(function() {
+                            s1error.text(" ");
+                        });           
+                    }
+                    else{
+                        stateflag = true;
+
+                    }
+                }
+                 return {
+                   checkCountry : checkCountry,
+                   checkState   : checkState,
+                };
+            })();
+
+
+            cityValidate = (function(){
+                //City validation
+                function checkCity(){
+                    if((city1.val()).trim() == ""){
+                        cityflag = false;
+                        cterror.text("please enter Your City Name");
+                        city1.focus();
+                        city1.change(function() {
+                            cterror.text(" ");
+                        });           
+                    }
+                    else if (!(city1.val()).match(cityreg)){ 
+                        cityflag = false;            
+                        cterror.text("special characters not allowed")
+                        city1.focus();
+                        city1.change(function() {
+                            cterror.text(" ");
+                        });                                
+                    }
+                    else{
+                        cityflag = true; 
+
+                    }
+               }return {
+                   checkCity : checkCity
+
+               };
+               
+           })();
+           captchaValidate = (function (){
+               var expr = $('#exprsn');
+               var result = $('#result').val();
+               var resultmsg = $('#resulterror');
+               var actual;
+               function verifyCaptcha(){
+                   actual =Math.round(eval(expr.val()))
+                    if (result.trim() == ""){
+                        resflag = false;
+                        resultmsg.text("Please Enter captcha result");
+                    }
+                    else{                   
+                        if (actual == result){
+
+                            resultmsg.text("");
+                            resflag = true;
+                        }
+                        else{
+                            resflag = false;
+                            resultmsg.text("Wrong Captcha Result");
+
+                        }
+                    }
+           }
+            return {
+                verifyCaptcha      :  verifyCaptcha
+            };
+
+        })();
+            validateAddress = addressValidate;
+            validateAddress. checkaddress();
+            validateCountryState =   countryStateValidate;
+            validateCountryState.checkCountry();
+            validateCountryState.checkState();
+            validateCity   = cityValidate;
+            validateCity.checkCity()
+            validateCaptcha = captchaValidate;
+            validateCaptcha.verifyCaptcha()
+            $("#result").change(function(){
+                validateCaptcha.verifyCaptcha()
+            })
         }
-        
 
-       return{
-            
-            noValueError       :  noValueError,
-            checkGrp1          :  checkGrp1,
-            generateCountries  :  generateCountries,
-            generateCaptcha    :  generateCaptcha,
-            getStates           :  getStates,
-            grp2Validate       :   grp2Validate,
-            
+        function grp2CompletenessCheck(){
 
+            if(addressflag && countryflag && stateflag && cityflag && resflag){
+              
+                return true;
+            }
+            else{
+               
+                return false;
+            }
+        }
+        return{
+            
+            noValueError         :  noValueError,
+            checkGrp1            :  checkGrp1,
+            generateCountries    :  generateCountries,
+            generateCaptcha      :  generateCaptcha,
+            getStates            :  getStates,
+            grp2Validate         :  grp2Validate,
+            showGrp1             :  showGrp1,
+            grp2CompletenessCheck : grp2CompletenessCheck
         };
 
       })();
-
       var v = validation;
       next= $('#next')
     
        next.click(function(){
           v.checkGrp1();
-
-      });
+       });
 
       $('input').blur(function(e){
           var pos=$(this).attr('id');
-
           v.noValueError(pos)
       })
+      $('select').blur(function(e){
+          var selectobj =$(this)
+          if(selectobj.val() == 0){
+              selectobj.next().text("please select value")
+          }
+          else{
+            selectobj.next().text(" ")
+          }
+        })
       v.generateCountries();
       v.generateCaptcha();
       $(".refrsh").click(function(){
@@ -386,19 +531,19 @@ $(document).ready(function(){
       $("#country2").change(function(){
         v.getStates("country2","state2")
       })
-
-
-
-
+      $("#prev").click(function(){
+          v.showGrp1()
+      })    
       $('form').submit(function(){
-       
-
-        
-      
+          v.grp2Validate();
+          flag = v.grp2CompletenessCheck()
+          if(flag == false){
+             return false;
+            }
+            else{
+             return  true;
+            }        
         })
-
-
-
 })
 
       
